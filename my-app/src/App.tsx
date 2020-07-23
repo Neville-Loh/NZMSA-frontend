5c42ae4435befbc8238179f98afa7cf5d2b7dde9
Neville <neville.code@gmail.com>
2020-07-23 13:56:17 +1200

    added search bar and mediaCard

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from './Components/SearchBarComponent/SearchBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <SearchBar/>
     </div>
   );
 }
